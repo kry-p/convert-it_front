e402e560871412b82918a300a1ed305c47cb1a31
kry-p <jhjung.dev@gmail.com>
2021-09-02 12:39:02 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./app.css";
+
+const App = () => <h1>test</h1>;
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const PORT = process.env.PORT || 3000;
+const outputDirectory = "./build";
+
+module.exports = {
+  entry: "./src/index.jsx",
+  output: {
+    path: path.resolve(__dirname, outputDirectory),
+    publicPath: "/",
+    filename: "[name].[fullhash].js",
+  },
+  devServer: {
+    port: PORT,
+    open: true,
+    proxy: {
+      "/api": `http://localhost:${PORT}`,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      minify: false,
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
+};
