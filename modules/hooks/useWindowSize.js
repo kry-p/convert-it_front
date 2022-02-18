17bdfe93af4e790232a9c5bb08dd4aa3dc8f5cfe
kry-p <jhjung.dev@gmail.com>
2022-02-19 00:08:29 +0900

    [ADD] Window size 처리 Hook 추가

---
+++ modules/hooks/useWindowSize.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+
+export default function useWindowSize() {
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  const handleResize = () => {
+    setWindowSize({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  };
+
+  // eslint-disable-next-line consistent-return
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('resize', handleResize);
+      return () => window.removeEventListener('resize', handleResize);
+    }
+  }, []);
+
+  return windowSize;
+}
